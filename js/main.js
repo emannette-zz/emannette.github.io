93562ed37b0c93b2855c76b0e74ac1e103190371
ethan <ethanmannette@gmail.com>
2016-04-01 17:44:11 -0600

    created main.js file

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var acc = document.getElementsByClassName('accordion');
+
+function accordion(sectionID) {
+
+}
+
+acc.onclick = function() {
+  console.log(acc.length);
+  console.log('test');
+};
