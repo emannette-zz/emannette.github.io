6908292c2c9c25aff728f86cde4d87c00e61a618
ethan <ethanmannette@gmail.com>
2016-04-18 13:45:47 -0600

    Created click events for moon, sun, and the light switch label for a few animations. Created a click event for the light switch to toggle the moon, determine if the projects should be hidden. Created click event for the projects tab to move the sun and light switch up. Created click events for project scroll arrows to scroll through projects if user wishes to use.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,63 @@
-var acc = document.getElementsByClassName('accordion');
+$(document).ready(function(){
 
-function accordion(sectionID) {
+  function hideProjects() {
+    if (!$('#lightSwitch').attr('checked')) {
+      $('#projectsTab').prop('checked', false);
+    }
+  }
 
-}
+  function pullCord() {
+    $('#lightSwitchLabel').animate({height: '150px'}, 300, 'linear').delay(250).animate({height: '120px'}, 300, 'linear');
+  }
 
-acc.onclick = function() {
-  console.log(acc.length);
-  console.log('test');
-};
+  $('#moon').click(function() {
+    pullCord();
+    $('#lightSwitch').trigger('click');
+  });
+  $('#sun').click(function() {
+    pullCord();
+    $('#lightSwitch').trigger('click');
+  });
+  $('#lightSwitchLabel').click(function() {
+    pullCord();
+    $('#lightSwitch').trigger('click');
+  });
+
+
+  $('#lightSwitch').click(function() {
+    // $('.about').toggle();
+    $('#moon').toggle();
+    $('#moonGlow').toggle();
+    hideProjects();
+    if ($('#lightSwitch').is(':checked')) {
+      $(document.body).css('background-color', '#63a4e9');
+    } else {
+      $(document.body).css('background-color', '#031528')
+    }
+  });
+
+  $('.projects-scroll-right').click(function() {
+    var pos = $('#projectsContainer').scrollLeft();
+    $('#projectsContainer').animate({
+      scrollLeft: pos += 1205
+    }, 800);
+    console.log(pos);
+
+  });
+
+  $('.projects-scroll-left').click(function() {
+    var pos = $('#projectsContainer').scrollLeft();
+    $('#projectsContainer').animate({
+      scrollLeft: pos -= 1205
+    }, 800);
+    console.log(pos);
+
+  });
+
+  $('#projectsTab').click(function() {
+      $('#sun').css('transition-duration', '2s');
+      $('#sunGlow').css('transition-duration', '2s');
+      $('#lightSwitchLabel').css('transition-duration', '2s');
+  });
+
+});
