aa1fb88e18a889bdec5e0c6f1533108a1025432f
ethan <ethanmannette@gmail.com>
2016-04-28 00:22:57 -0600

    Created function to determine which project will be visible when scrolling in the project div. Added click event for the petal wrapper. Created click events for scroll arrows within the project div. Created additional initial transitions for the first project. Added a bind command to remove mouse scroll functionality.

--- js/main.js
+++ js/main.js
@@ -6,9 +6,6 @@ $(document).ready(function(){
     }
   }
 
-  // function pullCord() {
-  //   $('#lightSwitchLabel').animate({height: '150px'}, 300, 'linear').delay(250).animate({height: '120px'}, 300, 'linear');
-  // }
 
   var fadeSun = function() {
     $('#sun').fadeTo(1000, 0);
@@ -22,58 +19,147 @@ $(document).ready(function(){
   }
 
 
-  $('#moon').click(function() {
-    // pullCord();
-    $('#lightSwitch').trigger('click');
-  });
-  $('#sun').click(function() {
-    // pullCord();
-    $('#lightSwitch').trigger('click');
-  });
-  // $('#lightSwitchLabel').click(function() {
-  //   // pullCord();
-  //   $('#lightSwitch').trigger('click');
-  // });
+  var projectVisibility = function(position) {
+    switch (position) {
+      case 0:
+        $('#endlessRunner')
+          .css('opacity', '0')
+          .css('transition-duration', '1500ms')
+          .css('transition-timing-function', 'ease');
+        $('#marioQuiz')
+          .css('opacity', '1')
+          .css('transition-duration', '1500ms')
+          .css('transition-timing-function', 'ease');
+        break;
+      case 1640:
+        $('#marioQuiz')
+          .css('opacity', '0')
+          .css('transition-duration', '1500ms')
+          .css('transition-timing-function', 'ease');
+        $('#fibonacci')
+          .css('opacity', '1')
+          .css('transition-duration', '1500ms')
+          .css('transition-timing-function', 'ease');
+      break;
+      case 3280:
+        $('#fibonacci')
+          .css('opacity', '0')
+          .css('transition-duration', '1500ms')
+          .css('transition-timing-function', 'ease');
+        $('#fractals')
+          .css('opacity', '1')
+          .css('transition-duration', '1500ms')
+          .css('transition-timing-function', 'ease');
+      break;
+      case 4920:
+        $('#fractals')
+          .css('opacity', '0')
+          .css('transition-duration', '1500ms')
+          .css('transition-timing-function', 'ease');
+        $('#gitStats')
+          .css('opacity', '1')
+          .css('transition-duration', '1500ms')
+          .css('transition-timing-function', 'ease');
+      break;
+      case 6560:
+        $('#gitStats')
+          .css('opacity', '0')
+          .css('transition-duration', '1500ms')
+          .css('transition-timing-function', 'ease');
+        $('#translator')
+          .css('opacity', '1')
+          .css('transition-duration', '1500ms')
+          .css('transition-timing-function', 'ease');
+      break;
+      default:
+        $('.project-content')
+          .css('opacity', '0')
+          .css('transition-duration', '1500ms')
+          .css('transition-timing-function', 'ease');
+        $('#endlessRunner')
+          .css('opacity', '1')
+          .css('transition-duration', '1500ms')
+          .css('transition-timing-function', 'ease');
+      break;
+    }
+  }
 
 
-  $('#lightSwitch').click(function() {
-    // $('#moon').toggle();
-    // $('#moonGlow').toggle();
-    hideProjects();
-    if ($('#lightSwitch').is(':checked')) {
-      opaqueSun();
+  $('#moon')
+    .click(function() {
+      $('#lightSwitch').trigger('click');
+  });
+  $('#sun')
+    .click(function() {
+      $('#lightSwitch').trigger('click');
+  });
+  $('#petalWrapper')
+    .click(function() {
+      $('#projectsTab').trigger('click');
+  });
+
+
+  $('#lightSwitch')
+    .click(function() {
+      hideProjects();
+      if ($('#lightSwitch').is(':checked')) {
+        opaqueSun();
+      } else {
+        fadeSun();
+      }
+  });
+
+
+  $('.scroll').click(function() {
+    projectVisibility($('#projectsContainer').scrollLeft())
+  })
+
+
+  $('.projects-scroll-right')
+    .click(function() {
+      var pos = $('#projectsContainer').scrollLeft();
+      $('#projectsContainer').animate({
+        scrollLeft: pos += window.innerWidth + 200
+      }, 800);
+      console.log(pos);
+  });
+
+
+  $('.projects-scroll-left')
+    .click(function() {
+      var pos = $('#projectsContainer').scrollLeft();
+      $('#projectsContainer').animate({
+        scrollLeft: pos -= window.innerWidth + 200
+      }, 800);
+      console.log(pos);
+  });
+
+
+  $('#projectsTab')
+    .click(function() {
+      $('#sun').css('transition-duration', '2s');
+      $('#sunGlow').css('transition-duration', '2s');
+    if($('#projectsTab').is(':checked')) {
+      $('#endlessRunner')
+        .css('opacity', '1')
+        .css('transition-delay', '2s')
+        .css('transition-duration', '1s');
     } else {
-      fadeSun();
+      $('#endlessRunner')
+        .css('opacity', '0')
+        .css('transition-duration', '.2s');
     }
   });
 
-  $('.projects-scroll-right').click(function() {
-    var pos = $('#projectsContainer').scrollLeft();
-    $('#projectsContainer').animate({
-      scrollLeft: pos += 1205
-    }, 800);
-    console.log(pos);
-
-  });
-
-  $('.projects-scroll-left').click(function() {
-    var pos = $('#projectsContainer').scrollLeft();
-    $('#projectsContainer').animate({
-      scrollLeft: pos -= 1205
-    }, 800);
-    console.log(pos);
-
-  });
-
-  $('#projectsTab').click(function() {
-    $('#sun').css('transition-duration', '2s');
-    $('#sunGlow').css('transition-duration', '2s');
-    $('#lightSwitchLabel').css('transition-duration', '2s');
-  });
 
   (function() {
     $('#sun').css('opacity', '0');
     $('#sunGlow').css('opacity', '0');
-  })()
+    $('#projectsContainer')
+      .bind('mousewheel', function() {
+        return false;
+    });
+  })();
+
 
 });
