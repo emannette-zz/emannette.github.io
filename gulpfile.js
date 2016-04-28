19ce3beb3ea4d2f6fd0be1f35f412c4d274a3a9c
ethan <ethanmannette@gmail.com>
2016-04-28 01:29:33 -0600

    Created tasks to watch for changes in sass files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,44 @@
+'use strict';
+
 var gulp = require('gulp'),
     sass = require('gulp-sass');
+
+var paths = {
+  scripts: [
+    './js/*.js'
+  ],
+  styles: [
+    './styles/*.css'
+  ]
+};
+
+
+gulp.task('lights', function() {
+  return gulp.src('scss/lights.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('styles'));
+});
+
+gulp.task('projects', function() {
+  return gulp.src('scss/projects.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('styles'));
+});
+
+gulp.task('terrain', function() {
+  return gulp.src('scss/terrain.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('styles'));
+});
+
+gulp.task('main', function() {
+  return gulp.src('scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('styles'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./scss/*.scss', ['lights', 'projects', 'terrain', 'main']);
+});
+
+gulp.task('default', ['sass:watch']);
