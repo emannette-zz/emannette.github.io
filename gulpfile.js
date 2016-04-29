79bdc5936f9c9662e6a47f5bf71575bfc8362a41
ethan <ethanmannette@gmail.com>
2016-04-28 20:01:46 -0600

    Refactored gulpfile so one task will watch for changes on all scss files.

--- gulpfile.js
+++ gulpfile.js
@@ -12,33 +12,14 @@ var paths = {
   ]
 };
 
-
-gulp.task('lights', function() {
-  return gulp.src('scss/lights.scss')
+gulp.task('styles', function() {
+  return gulp.src(paths.styles)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('styles'));
 });
 
-gulp.task('projects', function() {
-  return gulp.src('scss/projects.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('styles'));
+gulp.task('watch', function() {
+  gulp.watch(paths.styles, ['styles']);
 });
 
-gulp.task('terrain', function() {
-  return gulp.src('scss/terrain.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('styles'));
-});
-
-gulp.task('main', function() {
-  return gulp.src('scss/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('styles'));
-});
-
-gulp.task('sass:watch', function() {
-  gulp.watch('./scss/*.scss', ['lights', 'projects', 'terrain', 'main']);
-});
-
-gulp.task('default', ['sass:watch']);
+gulp.task('default', ['watch']);
