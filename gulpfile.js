00887e0e0c960fd988fab75fea766cfb38c469ea
ethan <ethanmannette@gmail.com>
2016-04-28 22:20:01 -0600

    Created tasks for nodemon and browser-sync but have not included them into the default gulp task. Still needs work.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    nodemon = require('gulp-nodemon');
 
 var paths = {
   scripts: [
@@ -13,20 +16,50 @@ var paths = {
   ]
 };
 
+var nodemonConfig = {
+  ext: 'html js css',
+  ignore: ['node_modules']
+};
+
+gulp.task('nodemon', function(cb) {
+  var called = false;
+  return nodemon(nodemonConfig)
+    .on('start', function() {
+      if (!called) {
+        called = true;
+        cb();
+      }
+    })
+    .on('restart', function() {
+      setTimeout(function() {
+        reload({ stream: false });
+      }, 1000);
+    });
+});
+
+gulp.task('browser-sync', ['nodemon'], function(done) {
+  browserSync({
+    proxy: 'localhost:3000',
+    port: 5000,
+    files:  ['.{js, css}'],
+    notify: true
+  }, done);
+});
+
 gulp.task('lint', function() {
   return gulp.src(paths.scripts)
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('styles', function() {
+gulp.task('sass', function() {
   return gulp.src(paths.styles)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('styles'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.styles,  ['styles'] );
+  gulp.watch(paths.styles,  ['sass'] );
   gulp.watch(paths.scripts, ['lint']   );
 });
 
